b9daa254968a14ddb0db1f64c29dddb76e6648f1
Max Rebuschatis <lincolnfrog@google.com>
2017-08-18 15:46:35 -0700

    Cleanup for hitTest
    Change-Id: Ifc7093a868708973b96ac63328c4d8a73e765ac6

--- android_webview/test/shell/tango/jni/TangoHandler.cpp
+++ android_webview/test/shell/tango/jni/TangoHandler.cpp
@@ -56,129 +56,6 @@ void onTangoEventAvailable(void* context, const TangoEvent* event) {
   tangoHandler->onTangoEventAvailable(event);
 }
 
-inline void multiplyMatrixWithVector(const float* m, const double* v, double* vr, bool addTranslation = true) {
-  double v0 = v[0];
-  double v1 = v[1];
-  double v2 = v[2];
-    vr[0] = m[ 0] * v0 + m[ 4] * v1 + m[ 8] * v2 + (addTranslation ? m[12] : 0);
-    vr[1] = m[ 1] * v0 + m[ 5] * v1 + m[ 9] * v2 + (addTranslation ? m[13] : 0);
-    vr[2] = m[ 2] * v0 + m[ 6] * v1 + m[10] * v2 + (addTranslation ? m[14] : 0);
-}
-
-inline void matrixInverse(const float* m, float* o)
-{
-    // based on http://www.euclideanspace.com/maths/algebra/matrix/functions/inverse/fourD/index.htm
-    float* te = o;
-    const float* me = m;
-
-    float n11 = me[ 0 ], n21 = me[ 1 ], n31 = me[ 2 ], n41 = me[ 3 ],
-      n12 = me[ 4 ], n22 = me[ 5 ], n32 = me[ 6 ], n42 = me[ 7 ],
-      n13 = me[ 8 ], n23 = me[ 9 ], n33 = me[ 10 ], n43 = me[ 11 ],
-      n14 = me[ 12 ], n24 = me[ 13 ], n34 = me[ 14 ], n44 = me[ 15 ],
-
-      t11 = n23 * n34 * n42 - n24 * n33 * n42 + n24 * n32 * n43 - n22 * n34 * n43 - n23 * n32 * n44 + n22 * n33 * n44,
-      t12 = n14 * n33 * n42 - n13 * n34 * n42 - n14 * n32 * n43 + n12 * n34 * n43 + n13 * n32 * n44 - n12 * n33 * n44,
-      t13 = n13 * n24 * n42 - n14 * n23 * n42 + n14 * n22 * n43 - n12 * n24 * n43 - n13 * n22 * n44 + n12 * n23 * n44,
-      t14 = n14 * n23 * n32 - n13 * n24 * n32 - n14 * n22 * n33 + n12 * n24 * n33 + n13 * n22 * n34 - n12 * n23 * n34;
-
-    float det = n11 * t11 + n21 * t12 + n31 * t13 + n41 * t14;
-
-    assert(det != 0);
-
-    float detInv = 1.0 / det;
-
-    te[ 0 ] = t11 * detInv;
-    te[ 1 ] = ( n24 * n33 * n41 - n23 * n34 * n41 - n24 * n31 * n43 + n21 * n34 * n43 + n23 * n31 * n44 - n21 * n33 * n44 ) * detInv;
-    te[ 2 ] = ( n22 * n34 * n41 - n24 * n32 * n41 + n24 * n31 * n42 - n21 * n34 * n42 - n22 * n31 * n44 + n21 * n32 * n44 ) * detInv;
-    te[ 3 ] = ( n23 * n32 * n41 - n22 * n33 * n41 - n23 * n31 * n42 + n21 * n33 * n42 + n22 * n31 * n43 - n21 * n32 * n43 ) * detInv;
-
-    te[ 4 ] = t12 * detInv;
-    te[ 5 ] = ( n13 * n34 * n41 - n14 * n33 * n41 + n14 * n31 * n43 - n11 * n34 * n43 - n13 * n31 * n44 + n11 * n33 * n44 ) * detInv;
-    te[ 6 ] = ( n14 * n32 * n41 - n12 * n34 * n41 - n14 * n31 * n42 + n11 * n34 * n42 + n12 * n31 * n44 - n11 * n32 * n44 ) * detInv;
-    te[ 7 ] = ( n12 * n33 * n41 - n13 * n32 * n41 + n13 * n31 * n42 - n11 * n33 * n42 - n12 * n31 * n43 + n11 * n32 * n43 ) * detInv;
-
-    te[ 8 ] = t13 * detInv;
-    te[ 9 ] = ( n14 * n23 * n41 - n13 * n24 * n41 - n14 * n21 * n43 + n11 * n24 * n43 + n13 * n21 * n44 - n11 * n23 * n44 ) * detInv;
-    te[ 10 ] = ( n12 * n24 * n41 - n14 * n22 * n41 + n14 * n21 * n42 - n11 * n24 * n42 - n12 * n21 * n44 + n11 * n22 * n44 ) * detInv;
-    te[ 11 ] = ( n13 * n22 * n41 - n12 * n23 * n41 - n13 * n21 * n42 + n11 * n23 * n42 + n12 * n21 * n43 - n11 * n22 * n43 ) * detInv;
-
-    te[ 12 ] = t14 * detInv;
-    te[ 13 ] = ( n13 * n24 * n31 - n14 * n23 * n31 + n14 * n21 * n33 - n11 * n24 * n33 - n13 * n21 * n34 + n11 * n23 * n34 ) * detInv;
-    te[ 14 ] = ( n14 * n22 * n31 - n12 * n24 * n31 - n14 * n21 * n32 + n11 * n24 * n32 + n12 * n21 * n34 - n11 * n22 * n34 ) * detInv;
-    te[ 15 ] = ( n12 * n23 * n31 - n13 * n22 * n31 + n13 * n21 * n32 - n11 * n23 * n32 - n12 * n21 * n33 + n11 * n22 * n33 ) * detInv;
-}
-
-inline void matrixTranspose(const GLfloat* m, GLfloat* o)
-{
-    if (o == m)
-    {
-        GLfloat t;
-        t = m[1];
-        o[1] = m[4];
-        o[4] = t;
-        t = m[2];
-        o[2] = m[8];
-        o[8] = t;
-        t = m[3];
-        o[3] = m[12];
-        o[12] = t;
-        t = m[6];
-        o[6] = m[9];
-        o[9] = t;
-        t = m[7];
-        o[7] = m[13];
-        o[13] = t;
-        t = m[11];
-        o[11] = m[14];
-        o[14] = t;
-    }
-    else
-    {
-        o[1] = m[4];
-        o[4] = m[1];
-        o[2] = m[8];
-        o[8] = m[2];
-        o[3] = m[12];
-        o[12] = m[3];
-        o[6] = m[9];
-        o[9] = m[6];
-        o[7] = m[13];
-        o[13] = m[7];
-        o[11] = m[14];
-        o[14] = m[11];
-    }
-    o[0] = m[0];
-    o[5] = m[5];
-    o[10] = m[10];
-    o[15] = m[15];
-}
-
-inline double dot(const double* v1, const double* v2)
-{
-  return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
-}
-
-inline void transformPlane(const double* p, const float* m, double* pr)
-{
-  double pCopy[3] = { p[0], p[1], p[2] };
-
-  pr[0] = p[0] * -p[3];
-  pr[1] = p[1] * -p[3];
-  pr[2] = p[2] * -p[3];
-
-  multiplyMatrixWithVector(m, pr, pr);
-  float mCopy[16];
-  double normal[3];
-  matrixInverse(m, mCopy);
-  matrixTranspose(mCopy, mCopy);
-  multiplyMatrixWithVector(mCopy, pCopy, normal, false);
-
-  pr[3] = -dot(pr, normal);
-  pr[0] = normal[0];
-  pr[1] = normal[1];
-  pr[2] = normal[2];
-}
-
 void matrixFrustum(float const & left,
              float const & right,
              float const & bottom,
@@ -235,7 +112,7 @@ mat4 SS_T_GL;
 mat4 SS_T_GL_INV;
 
 mat4 mat4FromTranslationOrientation(double *translation, double *orientation) {
-  vec3 translationV3 = vec3((float)translation[0], 
+  vec3 translationV3 = vec3((float)translation[0],
   (float)translation[1], (float)translation[2]);
   quat orientationQ = quat((float)orientation[0], (float)orientation[1],
   (float)orientation[2], (float)orientation[3]);
@@ -281,15 +158,15 @@ mat4 mat4FromTranslationOrientation(double *translation, double *orientation) {
   return m;
 };
 
-float rayIntersectsPlane(
-  vec3 planeNormal, vec3 planePosition, vec3 rayOrigin, vec3 rayDirection) {
+float rayIntersectsPlane(const vec3 &planeNormal, const vec3 &planePosition,
+    const vec3 &rayOrigin, const vec3 &rayDirection) {
   float denom = glm::dot(planeNormal, rayDirection);
   vec3 rayToPlane = planePosition - rayOrigin;
   return glm::dot(rayToPlane, planeNormal) / denom;
 }
 
-vec3 transformVec3ByMat4(vec3 v, mat4 m4) {
-  float* m;
+vec3 transformVec3ByMat4(const vec3 &v, const mat4 &m4) {
+  const float* m;
   m = value_ptr(m4);
 
   float x = v[0];
@@ -305,6 +182,52 @@ vec3 transformVec3ByMat4(vec3 v, mat4 m4) {
     (m[0] * x + m[4] * y + m[8] * z + m[12]) / w,
     (m[1] * x + m[5] * y + m[9] * z + m[13]) / w,
     (m[2] * x + m[6] * y + m[10] * z + m[14]) / w);
+}
+
+void setFloat16FromMat4(float *out, const mat4 &m4) {
+  const float* fM = value_ptr(m4);
+  for (int i = 0; i < 16; i++) {
+    out[i] = fM[i];
+  }
+}
+
+bool isPointInPolygon(const vec3 &point, const vec3* polygonPoints, int count) {
+  if (count < 3)
+  {
+    return false;
+  }
+
+  LOGE("POINT: (%f, %f, %f)", point.x, point.y, point.z);
+  LOGE("POLY");
+  vec3 lastUp = vec3(0, 0, 0);
+  for (int i = 0; i < count; ++i)
+  {
+    LOGE("(%f, %f, %f", polygonPoints[i].x, polygonPoints[i].y, polygonPoints[i].z);
+    vec3 v0 = point - polygonPoints[i];
+    vec3 v1;
+    if (i == count - 1)
+    {
+      v1 = polygonPoints[0] - polygonPoints[i];
+    }
+    else
+    {
+      v1 = polygonPoints[i + 1] - polygonPoints[i];
+    }
+
+    vec3 up = glm::cross(v0, v1);
+    if (i != 0)
+    {
+      float sign = glm::dot(up, lastUp);
+      if (sign < 0)
+      {
+        return false;
+      }
+    }
+
+    lastUp = up;
+  }
+  LOGE("/POLY");
+  return true;
 }
 
 } // End anonymous namespace
@@ -337,7 +260,8 @@ TangoHandler::TangoHandler(): connected(false)
   , cameraImageTextureHeight(0)
   , textureIdConnected(false)
 {
-  // This matrix comes from the tango team to transform from tango-space to GL-space.
+  // SS means Start of Service: this matrix does the conversion from Start of Service transform
+  // to OpenGL (tango-space has z as the up-vector, not y).
   float ss_t_gl[16] = {1, 0, 0, 0, 0, 0, 1, 0, 0, -1, 0, 0, 0, 0, 0, 1};
   SS_T_GL = make_mat4(ss_t_gl);
   SS_T_GL_INV = inverse(SS_T_GL);
@@ -602,6 +526,8 @@ bool TangoHandler::hitTest(float x, float y, std::vector<Hit>& hits)
       float* fM;
       mat4 projectionMatrix;
       fM = value_ptr(projectionMatrix);
+      // TODO(lincolnfrog): near and far values should not be constants. Use the values
+      // supplied by the client.
       getProjectionMatrix(0.01, 10000, fM);
 
       // Set the model view matrix.
@@ -630,29 +556,34 @@ bool TangoHandler::hitTest(float x, float y, std::vector<Hit>& hits)
       // Check each plane for intersections.
       for (int i = 0; i < numberOfPlanes; i++) {
         TangoPlaneData planeData = planes[i];
+        if (!planeData.is_valid) {
+          // This plane is no longer valid, so skip it.
+          continue;
+        }
+        if (planeData.subsumed_by != -1) {
+          // This plane has been subsumed by another plane, so skip it.
+          continue;
+        }
+
         TangoPoseData planePose = planeData.pose;
 
         // Get the plane transform matrix.
         mat4 planeMatrix = mat4FromTranslationOrientation(
             planePose.translation, planePose.orientation);
-        
+
         // Plane is in the tango coordinates, so transform into GL coordinate space.
         planeMatrix = SS_T_GL_INV * planeMatrix * SS_T_GL;
-
-        LOGE("Plane Pose Translation: (%f, %f, %f)", 
-            planePose.translation[0], planePose.translation[1], planePose.translation[2]);
 
         // Get the plane center in world-space.
         vec3 planeCenter = vec3(planeData.center_x, 0, planeData.center_y);
         vec3 planePosition = transformVec3ByMat4(planeCenter, planeMatrix);
-        LOGE("Plane Position: (%f,%f,%f)", planePosition.x, planePosition.y, planePosition.z);
 
         // Assume all planes are oriented horizontally.
         vec3 planeNormal = vec3(0, 1, 0);
-        
+
         // Check if the ray intersects the plane.
         float t = rayIntersectsPlane(planeNormal, planePosition, worldRayOrigin, worldRayDirection);
-        
+
         // If t < 0, there is no intersection.
         if (t < 0) {
           continue;
@@ -666,29 +597,42 @@ bool TangoHandler::hitTest(float x, float y, std::vector<Hit>& hits)
         vec3 planeIntersectionLocal = transformVec3ByMat4(planeIntersection, planeMatrixInv);
 
         // Check if the intersection is outside of the extent of the plane.
-        // TODO: use the actual polygon instead of the rough bounding box!
         if (abs(planeIntersectionLocal.x - planeData.center_x) > planeData.width) {
           continue;
         }
-        if (abs(planeIntersectionLocal.z - planeData.center_y) > planeData.height) {
+        if (abs(planeIntersectionLocal.y - planeData.center_y) > planeData.height) {
           continue;
         }
 
-        mat4 hitMatrix = translate(mat4(1.0f), planeIntersection);
-        fM = value_ptr(hitMatrix);
-        Hit hit;
-        for (int j = 0; j < 16; j++) {
-          hit.modelMatrix[j] = fM[j];
+        /*
+        // TODO(lincolnfrog): enable polygon test when we figure out why the polygon is so bad.s
+        // Transform all the points into world space using the plane matrix.
+        vec3 polygonPoints[planeData.boundary_point_num];
+        for (int i = 0; i < planeData.boundary_point_num; ++i) {
+          polygonPoints[i] = transformVec3ByMat4(
+            vec3((float)planeData.boundary_polygon[i * 2], 0, (float)planeData.boundary_polygon[(i * 2) + 1]), 
+            planeMatrix);
         }
+        
+        if(!isPointInPolygon(planeIntersection, polygonPoints, planeData.boundary_point_num)) {
+          LOGE("%i Rejected (POLY)", planeData.id);
+          continue;
+        }
+        */
+
+        mat4 hitMatrix = translate(mat4(1.0f), planeIntersection);
+        Hit hit;
+        setFloat16FromMat4(hit.modelMatrix, hitMatrix);
         hits.push_back(hit);
       }
 
+      // Sort the hits based on distance to the camera.
       auto sortFunc = [worldRayOrigin] (Hit a, Hit b) {
         vec3 vA = vec3(a.modelMatrix[12], a.modelMatrix[13], a.modelMatrix[14]);
-        float dA = glm::dot(vA, worldRayOrigin);
+        float dA = glm::length2(vA - worldRayOrigin);
 
         vec3 vB = vec3(b.modelMatrix[12], b.modelMatrix[13], b.modelMatrix[14]);
-        float dB = glm::dot(vB, worldRayOrigin);
+        float dB = glm::length2(vB - worldRayOrigin);
         return dA < dB;
       };
 
